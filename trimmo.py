1616fb8fea42b8c74c11055a04e59f4318e4f9ef
Yang Liu <thomasliuy@gmail.com>
2018-08-19 18:09:49 -0400

    modified trimmo.py

--- trimmo.py
+++ trimmo.py
@@ -12,16 +12,14 @@ outfq2 = infq2 + ".cleaned.fastq"
 outrmfq1 = infq1 + ".removed.fastq"
 outrmfq2 = infq2 + ".removed.fastq"
 logfile = infq1 + ".log"
-n_slots = cpu_count()
-print("infq1: ".format(infq1))
-print("infq2: ". format(infq2))
-print("adapterfa: ".format(adapterfa))
+n_threads = cpu_count()
+print("infq1: {}".format(infq1))
+print("infq2: {}". format(infq2))
+print("adapterfa: {}".format(adapterfa))
 
-command = "java -jar {}/trimmomatic-0.36.jar PE -threads {} -phread33 -trimlog {} {} {} {} {} {} {} ILLUMINACLIP:{}:3:30:7:1:TRUE LEADING:2 TRAILING:2 SLIDINGWINDOW:4:10 MINLEN:36".format(params)
-
-java -jar ${trimmomaticpath}/trimmomatic-0.36.jar PE -threads $NSLOTS -phred33 -trimlog $logfile $infq1 $infq2 $outfq1 $out\
-rmfq1 $outfq2 $outrmfq2 ILLUMINACLIP:${adapterfa}:3:30:7:1:TRUE LEADING:2 TRAILING:2 SLIDINGWINDOW:4:10 MINLEN:36
-
-command = java -jar trimmomatic-0.33.jar PE -phred33 input_forward.fq.gz input_reverse.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+command = "java -jar {0}/trimmomatic-0.36.jar PE -threads {1} -phread33 -trimlog \
+{2} {3} {4} {5} {6} {7} {8} ILLUMINACLIP:{9}:3:30:7:1:TRUE LEADING:2 TRAILING:2 \
+SLIDINGWINDOW:4:10 MINLEN:36".format(trimmomaticpath, n_threads, logfile, infq1, infq2, 
+outfq1, outrmfq1, outfq2, outrmfq2, adapterfa)
 
 os.system(command)
