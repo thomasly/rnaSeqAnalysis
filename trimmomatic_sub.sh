6d8f1221ddd8fa6af72c0425cb81de44a89cfe9d
Yang Liu <thomasliuy@gmail.com>
2018-08-20 12:18:37 -0400

    update trimmomatic_sub.sh

--- trimmomatic_sub.sh
+++ trimmomatic_sub.sh
@@ -2,10 +2,11 @@
 
 #$ -N rnaSeqFastqc
 #$ -cwd -V
+#$ -pe openmpi 4-16
 #$ -o /ifs/data/proteomics/projects/Sunny/YL/trimmomatic_outputs/$JOB_NAME_$JOB_ID.out
 #$ -e /ifs/data/proteomics/projects/Sunny/YL/trimmomatic_outputs/$JOB_NAME_$JOB_ID.err
 
 module load python/3.6.4
 
-python3 trimmo.py $SGE_TASK_ID
+python3 trimmo.py $1 $2
 # python3 fastqc_exe.py $1
