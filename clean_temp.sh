54c681f181d13c9a2ba92f5c8b99722902aeccfa
Sun <xs338@phoenix2.cm.cluster>
2018-08-21 14:10:48 -0400

    update clean_temp to make it output to qsuboutputs folder

--- clean_temp.sh
+++ clean_temp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-#$ -cwd
+#$ -cwd -V
+#$ -N cleaningTemp
+#$ -o /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.out
+#$ -e /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.err
 
 rm ./temp
--- trimmomatic_sub.sh
+++ trimmomatic_sub.sh
@@ -2,7 +2,7 @@
 
 #$ -N rnaSeqTimmomatic
 #$ -cwd -V
-## -pe openmpi 4-8
+#$ -pe openmpi 1-2
 #$ -o /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.out
 #$ -e /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.err
 
