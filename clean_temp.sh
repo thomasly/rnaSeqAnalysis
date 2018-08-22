f3be61fc830e46c742843f560f21143835b25fac
Yang Liu <thomasliuy@gmail.com>
2018-08-22 17:04:51 -0400

    update clean_temp.sh

--- clean_temp.sh
+++ clean_temp.sh
@@ -5,4 +5,4 @@
 #$ -o /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.out
 #$ -e /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.err
 
-rm ./temp
+rm ./*temp
