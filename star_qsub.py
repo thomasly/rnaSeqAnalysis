fb90550548ce0fe2d5d40a11fb62db0f818a0568
Yang Liu <thomasliuy@gmail.com>
2018-08-22 17:05:38 -0400

    qsub file for star

---
+++ star_qsub.py
@@ -0,0 +1,27 @@
+import os, sys
+from paths import RnaSeqPath
+
+def star_qsub():
+    """
+    qsub star mission through star_sub.sh
+    """
+
+    paths = RnaSeqPath()
+
+    try:
+        os.mkdir(os.path.join(paths.home, "star_outputs"))
+    except IOError:
+        pass
+
+    n_jobs = int(len(os.listdir(paths.fastq)) / 2)
+    job_arr = "-t 1-" + str(n_jobs)
+
+    command = "qsub {} star_sub.sh".format(job_arr)
+    os.system(command)
+
+    cleaning = "qsub -hold_jid STAR_job clean_temp.sh"
+    os.system(cleaning)
+
+
+if __name__ == "__main__":
+    star_qsub()
