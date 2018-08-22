4c76de3c85e18b975d798aa23b503d08680ea7ea
Yang Liu <thomasliuy@gmail.com>
2018-08-22 17:14:09 -0400

    bug fix star_sub.sh

--- star_sub.sh
+++ star_sub.sh
@@ -5,11 +5,9 @@
 #$ -pe openmpi 1-2
 #$ -o /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.out
 #$ -e /ifs/data/proteomics/projects/Sunny/YL/qsuboutputs/$JOB_NAME_$JOB_ID.err
-#$ -m ea
-#$ -M liuy08@nyumc.org
+
 
 module load python/3.6.4
 module load star
 
-python3 trimmo.py $SGE_TASK_ID
-# python3 fastqc_exe.py $1
+python3 star.py $SGE_TASK_ID
