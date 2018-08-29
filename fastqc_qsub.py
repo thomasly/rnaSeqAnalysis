246bb1233856c60802b92a89c0d7182c7eb92a58
Yang Liu <thomasliuy@gmail.com>
2018-08-29 15:06:50 -0400

    optimize fastqc_qsub.py

--- fastqc_qsub.py
+++ fastqc_qsub.py
@@ -1,6 +1,12 @@
 import os, sys
 from utils import generate_bash_file, qsub, clean
+from paths import RnaSeqPath
+from glob import glob
+from math import ceil
 
+paths = RnaSeqPath()
+
+# create the shell file for qsub
 sh_file = generate_bash_file(
     filename_base="fastqc",
     job_name="rnaSeqFastqc",
@@ -10,7 +16,14 @@ sh_file = generate_bash_file(
         "python3 fastqc.py $1"
     ]
 )
-for t in range(5):
+
+# calculate how many qsub to run
+threads = os.cpu_count()
+m = int(ceil(len(glob(os.path.join(paths.fastq, "*.fastq"))) / threads))
+
+# submit jobs to hpc
+for t in range(m):
     qsub(sh_file, [t])
 
+# cleaning the temporary shell files after main job is done
 qsub(clean(after="rnaSeqFastqc"))
