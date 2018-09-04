0b0d3debab64c2efab0783bfb2a9c18f54df7792
Yang Liu <thomasliuy@gmail.com>
2018-09-04 17:38:23 -0400

    run htseq file by file

--- htseq_qsub.py
+++ htseq_qsub.py
@@ -15,24 +15,27 @@ def htseq_count():
         pass
     
     bam_files = glob(os.path.join(paths.samtools_sorted, "*sorted.bam.bam"))
-    alignment_files = " ".join(bam_files)
+    # alignment_files = " ".join(bam_files)
     gff_file = paths.hg38_l1_annotation
-    output_file = os.path.join(paths.htseq_outputs, "htseq.out")
 
-    shell_file = generate_bash_file(
-        filename_base="htseq",
-        job_name="htseq_job",
-        commands=[
-            "module load anaconda3",
-            "source activate htseq",
-            "htseq-count -f bam -r name {} {} > {}".format(
-                alignment_files,
-                gff_file,
-                output_file
-            )
-        ]
-    )
-    qsub(shell_file)
+    for i, alignment_file in enumerate(bam_files):
+        output_file = os.path.join(
+            paths.htseq_outputs, "{}_htseq.out".format(alignment_file)
+        )
+        shell_file = generate_bash_file(
+            filename_base="htseq",
+            job_name="htseq_job_{}".format(i),
+            commands=[
+                "module load anaconda3",
+                "source activate htseq",
+                "htseq-count -f bam -r name {} {} > {}".format(
+                    alignment_file,
+                    gff_file,
+                    output_file
+                )
+            ]
+        )
+        qsub(shell_file)
 
 if __name__ == "__main__":
     htseq_count()
